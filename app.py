4eaa79e560ad153605f2d47a0950199c3f24a5d9
Abraao1231 <abraao1231@gmail.com>
2023-10-30 18:39:51 -0300

    implementacao de algoritimo

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, request ,render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    
-    def agendar_tarefas(funcionarios, tarefas):
+def agendar_tarefas(funcionarios, tarefas):
         funcionarios_ordenados = sorted(funcionarios, key=lambda x: x['carga_de_trabalho'])
         agendamento = []
 
@@ -19,29 +16,50 @@ def index():
             if funcionario_disponivel:
                 agendamento.append({
                     'tarefa': tarefa,
-                    'funcionario': funcionario_disponivel['nome']
+                    'funcionario': funcionario_disponivel['Nome']
                 })
                 funcionario_disponivel['carga_de_trabalho'] -= tarefa['carga_de_trabalho']
 
         return agendamento
 
-
-    funcionarios = [
-        {'nome': 'Funcionario1', 'carga_de_trabalho': 30},
-        {'nome': 'Funcionario2', 'carga_de_trabalho': 20},
-        {'nome': 'Funcionario3', 'carga_de_trabalho': 40},
-    ]
-
-    tarefas = [
-        {'nome': 'Tarefa1', 'carga_de_trabalho': 25},
-        {'nome': 'Tarefa2', 'carga_de_trabalho': 15},
-        {'nome': 'Tarefa3', 'carga_de_trabalho': 35},
-    ]
-
-    agendamento = agendar_tarefas(funcionarios, tarefas)
+@app.route('/')
+def index():
 
     # Renderize o template e retorne a saída HTML
-    return render_template('template.html', agendamento=agendamento)
+    return render_template('template.html')
+
+@app.route('/next-step', methods=['POST'])
+def renderNextStep():
+        qtdTarefas = int(request.form.get('qtd-tarefas'))
+        qtdFuncionarios = int(request.form.get('qtd-funcionarios'))
+        funcionarios = []
+
+        for i in range(0, qtdFuncionarios):
+           funcionarios.append(i)  
+
+        tarefas = []
+
+        for i in range(0, qtdTarefas):
+           tarefas.append(i)  
+
+       
+        return render_template('nextStep.html',  funcionarios=funcionarios, tarefas=tarefas)
+
+
+@app.route('/render-result', methods=['POST'])
+def renderResult():
+    funcionarios = []
+    tarefas = []
+    for chave, valor in request.form.to_dict().items():
+        if ('funcionario' in chave):    
+            funcionarios.append({'Nome': chave,'carga_de_trabalho': int(valor) })
+        if ('tarefa' in chave):    
+            tarefas.append({'Nome': chave,'carga_de_trabalho': int(valor) })
+            
+    result=agendar_tarefas(funcionarios, tarefas)
+   
+    
+    return render_template('result.html',  result=result)
 
 if __name__ == '__main__':
     app.run()
